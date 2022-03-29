e2c9bb7be1c01b94ee57cba0c2771d90f92411b5
jellymando <cicikdi@naver.com>
2022-03-29 21:15:54 +0900

    feat: storybook 공통 스타일 적용

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,21 @@
+import { Global } from "@emotion/react";
+import { Reset } from "styles";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+      date: /Date$/
+    }
+  }
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <Global styles={Reset} />
+      <Story />
+    </>
+  )
+];
